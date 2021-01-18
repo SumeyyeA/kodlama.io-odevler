9b314648f50c64603b94796e6051fc8402bdd520
Sümeyye Almas <72462370+SumeyyeA@users.noreply.github.com>
2021-01-19 00:46:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _3.gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            urun urun1 = new urun();
+            urun1.urunAdi = "klavye";
+            urun1.urunFiyati = 200;
+            urun1.urunAciklamasi = "pc için";
+
+            urun urun2 = new urun();
+            urun2.urunAdi = "fare";
+            urun2.urunFiyati = 100;
+            urun2.urunAciklamasi = "kablosuz";
+
+            urun[] urunler = new urun[] {urun1,urun2 };
+
+            foreach (urun urun in urunler)
+            {
+                Console.WriteLine(urun.urunAdi);
+                Console.WriteLine(urun.urunFiyati);
+                Console.WriteLine(urun.urunAciklamasi);
+                Console.WriteLine("---------------");
+            }
+
+            sepetManager Sepetmanager = new sepetManager();
+            Sepetmanager.Ekle(urun1);
+            Sepetmanager.Ekle(urun2);
+        }
+    }
+}
---
+++ sepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3.gun
+{
+    class sepetManager
+    {
+        public void Ekle(urun uruns){
+
+            Console.WriteLine("sepete eklendi"+uruns.urunAdi);
+
+
+
+        }
+    }
+}
