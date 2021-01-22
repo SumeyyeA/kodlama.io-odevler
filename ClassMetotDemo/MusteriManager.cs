af3dd21520913510b095da17f0f180534bae2043
Sümeyye Almas <72462370+SumeyyeA@users.noreply.github.com>
2021-01-22 13:24:08 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            
+            Console.WriteLine("musteri basariyla eklendi.");
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+            Console.WriteLine(musteri.CardNo);
+            Console.WriteLine("---------------");
+
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Musteri[] musteris = new Musteri[] {musteri };
+
+            foreach (Musteri item in musteris)
+            {
+                Console.WriteLine("musteriler listeleniyor");
+                Console.WriteLine(musteri.MusteriAdi+" "+musteri.MusteriSoyadi);
+                Console.WriteLine(musteri.CardNo);
+                Console.WriteLine("---------------");
+            }
+
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("musteri silindi:");
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi);
+            Console.WriteLine(musteri.CardNo);
+            Console.WriteLine("---------------");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "sumeyye";
+            musteri1.MusteriSoyadi = "almas";
+            musteri1.CardNo = 111111;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "engin";
+            musteri2.MusteriSoyadi = "demiroğ";
+            musteri2.CardNo = 222222;
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("MUSTERİLER");
+            musteriManager.MusteriListele(musteri1);
+
+            Console.WriteLine("MUSTERİ EKLE");
+            musteriManager.MusteriEkle(musteri1);
+
+            Console.WriteLine("MUSTERİ SİL");
+            musteriManager.MusteriSil(musteri2);
+
+            
+        }
+    }
+}
